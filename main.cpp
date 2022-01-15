553b2de5f7851051a69be2893f8b080c8886eee6
Eren Toy <erentoyfb@gmail.com>
2022-01-15 21:27:24 +0300

    Logo
    Application logo is added

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    a.setWindowIcon(QIcon("../Interface/resources/icon2.png"));
     w.show();
     return a.exec();
 }
