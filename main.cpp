81b500774aec1d1618be7ff7ad45cf8f9f909ff9
Eren Toy <erentoyfb@gmail.com>
2022-04-09 01:09:50 +0300

    Full Screen Ui and New Logo
    Logo is updated and Ui sizeis set to maximum

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    a.setWindowIcon(QIcon("D:/Projects/Interface/resources/icon2.png"));
-    w.show();
+    a.setWindowIcon(QIcon("D:/Projects/Interface/resources/dik.png"));
+    w.setWindowFlags(Qt::Window |Qt::WindowMaximizeButtonHint | Qt::WindowCloseButtonHint | Qt::WindowMinimizeButtonHint);
+    w.setWindowState(Qt::WindowMaximized);
+    w.show/*FullScreen*/();
     return a.exec();
 }
